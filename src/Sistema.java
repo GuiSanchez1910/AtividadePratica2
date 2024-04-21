0d1b620ef2ccc8ba4bc54b0bccec7fcf91831e2c
BrunoHunoff <brunohunoff@gmail.com>
2024-04-21 11:22:46 -0300

    Menu opções + tratamento de erro

--- src/Sistema.java
+++ src/Sistema.java
@@ -5,12 +5,14 @@ public class Sistema {
         System.out.println("1) Cadastrar atleta de Futebol");
         System.out.println("2) Cadastrar atleta de Vôlei");
         System.out.println("3) Cadastrar atleta de Basquete");
-        System.out.println("4) Listar atleta de Futebol");
-        System.out.println("5) Listar atleta de Vôlei");
-        System.out.println("6) Listar atleta de Basquete");
+        System.out.println("4) Listar atletas de Futebol");
+        System.out.println("5) Listar atletas de Vôlei");
+        System.out.println("6) Listar atletas de Basquete");
         System.out.println("7) Listar todos os atletas");
-        System.out.println("8) Excluir atleta");
-        System.out.println("9) Excluir todos os atletas");
+        System.out.println("8) Buscar atleta ");
+        System.out.println("9) Editar atleta ");
+        System.out.println("10) Excluir atleta");
+        System.out.println("11) Excluir todos os atletas");
         System.out.println("0) Sair");
         System.out.print("Selecione uma opção: ");
     }
@@ -47,16 +49,25 @@ public class Sistema {
                 break;
 
             case 8:
-                excluirAtleta();
+                buscarAtleta();
                 break;
 
             case 9:
+                editarAtleta();
+                break;
+
+            case 10:
+                excluirAtleta();
+                break;
+
+            case 11:
                 excluirTodosAtletas();
                 break;
 
             case 0:
                 System.out.println("Sistema finalizado.");
                 break;
+
             default:
                 System.out.println("Opção inválida! Tente novamente");
                 break;
@@ -234,6 +245,94 @@ public class Sistema {
             return;
         }
         System.out.println("Não foram encontrados atletas para excluir.");
+    }
+
+    private static void buscarAtleta() {
+        String nome;
+
+        System.out.println("\nBuscar atleta: \n");
+
+        System.out.print("Nome do atleta: ");
+        nome = Console.lerString();
+
+        for (Atleta temp: CadastrarAtleta.getListaAtletas()) {
+            if (nome.equals(temp.getNome())) {
+                System.out.println(temp);
+                return;
+            }
+        }
+
+        System.out.println("Atleta não encontrado.");
+    }
+
+    public static void editarAtleta() {
+        String nome;
+
+        System.out.println("\nEditar atleta: \n");
+
+        System.out.print("Nome do atleta: ");
+        nome = Console.lerString();
+
+        for (Atleta temp: CadastrarAtleta.getListaAtletas()) {
+            if (nome.equals(temp.getNome())) {
+                if (temp instanceof AtletaFutebol) {
+                    System.out.print("Nome: ");
+                    temp.setNome(Console.lerString());
+
+                    System.out.print("Idade: ");
+                    temp.setIdade(Console.lerInt());
+
+                    System.out.print("Time: ");
+                    temp.setTime(Console.lerString());
+
+                    System.out.print("Número de gols: ");
+                    ((AtletaFutebol) temp).setGols(Console.lerInt());
+
+                    System.out.print("Número de assistências: ");
+                    ((AtletaFutebol) temp).setAsssistencias(Console.lerInt());
+
+                    return;
+                }
+                if (temp instanceof AtletaVolei) {
+                    System.out.print("Nome: ");
+                    temp.setNome(Console.lerString());
+
+                    System.out.print("Idade: ");
+                    temp.setIdade(Console.lerInt());
+
+                    System.out.print("Time: ");
+                    temp.setTime(Console.lerString());
+
+                    System.out.print("Pontos: ");
+                    ((AtletaVolei) temp).setPontos(Console.lerInt());
+
+                    System.out.print("Número de bloqueios: ");
+                    ((AtletaVolei) temp).setBloqueios(Console.lerInt());
+
+                    return;
+                }
+                if (temp instanceof AtletaBasquete) {
+                    System.out.print("Nome: ");
+                    temp.setNome(Console.lerString());
+
+                    System.out.print("Idade: ");
+                    temp.setIdade(Console.lerInt());
+
+                    System.out.print("Time: ");
+                    temp.setTime(Console.lerString());
+
+                    System.out.print("Cestas de Dois Pontos: ");
+                    ((AtletaBasquete) temp).setPontosDeDois(Console.lerInt());
+
+                    System.out.print("Cestas de Três Pontos: ");
+                    ((AtletaBasquete) temp).setPontosDeTres(Console.lerInt());
+
+                    return;
+                }
+            }
+        }
+
+        System.out.println("Atleta não encontrado");
     }
 
     public static void executar() {
